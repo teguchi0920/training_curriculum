d24c656ba25da8ef7ec4ce1e0978a7f21b3f11fc
teguchi0920 <childrenkey@gmail.com>
2023-02-27 19:04:12 +0900

    曜日が正しく表示されるように修正

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -35,7 +35,7 @@ class CalendarsController < ApplicationController
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
 
-      wday_num = @todays_date.wday
+      wday_num = @todays_date.wday + x
       if wday_num >= 7 
         wday_num = wday_num -7
       end
@@ -43,6 +43,5 @@ class CalendarsController < ApplicationController
       days = { month: (@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans, wday: wdays[wday_num] }
       @week_days.push(days)
     end
-
   end
 end
